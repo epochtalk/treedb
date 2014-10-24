4f4e8eb1987d0e17543582550b6454fca650ba51
Bronson Oka <boka@slickage.com>
2014-10-23 23:57:50 -1000

    use path.join in examples/store

--- examples/store.js
+++ examples/store.js
@@ -1,5 +1,5 @@
 var path = require('path');
-var db = require('levelup')('/tmp/treedb');
+var db = require('levelup')(path.join('/', 'tmp', 'treedb'));
 var tree = require(path.join(__dirname, '..'))(db);
 tree.store({type: 'person', name: 'Foo Bar', slogan: 'hello world'}, function(err, ch) {
   console.log(ch);
