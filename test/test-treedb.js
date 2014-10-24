6eb67e86768106f213427d712a410e0404a7191d
Bronson Oka <boka@slickage.com>
2014-10-24 00:38:33 -1000

    use path.join in test/test-treedb

--- test/test-treedb.js
+++ test/test-treedb.js
@@ -1,8 +1,8 @@
 var path = require('path');
 var async = require('async');
 var levelup = require('levelup');
-var treedb = require('../');
-var db = new levelup(path.join('/tmp', '.treedb'));
+var treedb = require(path.join(__dirname, '..'));
+var db = new levelup(path.join('/', 'tmp', '.treedb'));
 var tree = treedb(db);
 
 module.exports = tree;
