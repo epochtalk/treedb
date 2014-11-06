fd37566fae9c5f3cf34cc9222476922a7a96605e
Bronson Oka <boka@slickage.com>
2014-11-06 05:11:49 -1000

    [metatreedb] epochmeta included in meta opts

--- test/test-treedb.js
+++ test/test-treedb.js
@@ -3,7 +3,7 @@ var async = require('async');
 var levelup = require('levelup');
 var treedb = require(path.join(__dirname, '..'));
 var db = new levelup(path.join('/', 'tmp', '.treedb'));
-var tree = treedb(db);
+var tree = treedb(db, {meta: require('epochmeta')});
 
 module.exports = tree;
 
