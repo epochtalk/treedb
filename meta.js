5f9877735b13ba143532666ecd9d05a9dd4b043c
Bronson Oka <boka@slickage.com>
2014-11-06 05:07:30 -1000

    [metatreedb] add meta from epochmeta project
    this is a generic driver that takes controllers
    and operations to do specific things for each usecase

---
+++ meta.js
@@ -0,0 +1,67 @@
+var path = require('path');
+var trigger = require('level-trigger');
+
+var Meta = module.exports = function(tree, stuff) {
+  var self = this;
+  self.tree = tree;
+  self.operations = stuff.operations;
+  self.operations.init(tree);
+  self.controllers = stuff.controllers;
+  trigger(tree.db, 'metadata-trigger', function(ch) {
+    var key = ch.key;
+    if (ch.type === 'put') {
+      // Get the type
+      var type = key[0];
+      // If a controller exists for the type
+      if (self.controllers[type]) {
+        // Create a new metadata for it
+        // using the controller's model
+        if (self.controllers[type].model) {
+          var value = new self.controllers[type].model();
+          var rows = [];
+          rows.push({type: 'put', key: key, value: value});
+          self.tree.meta.batch(rows, function(err) {
+            if (err) {
+              console.log('batch error: ' + key);
+            }
+            else {
+              self.tree.meta.get(key, function(err, value) {
+              });
+            }
+          });
+        }
+        // Call the onPut for the metadata
+        if (self.controllers[type].onPut) {
+          self.controllers[type].onPut(key);
+        }
+      };
+    }
+    else if (ch.type === 'del') {
+      // Get the type
+      var type = key[0];
+      // If a controller exists for the type
+      if (self.controllers[type]) {
+        // Delete the metadata for it
+        if (self.controllers[type].model) {
+          var rows = [];
+          rows.push({type: 'del', key: key});
+          self.tree.meta.batch(rows);
+        }
+        // Call the onDel for the metadata
+        if (self.controllers[type].onDel) {
+          self.controllers[type].onDel(key);
+        }
+      };
+    }
+    return ch.key;
+  }, function(value, done) { done(); });
+};
+
+// options:  key, callback, field
+Meta.prototype.get = function(options) {
+  var self = this;
+  var key = options.key;
+  self.operations.getValue({key: options.key, callback: function(err, value) {
+    options.callback(err, value[options.field]);
+  }});
+};
