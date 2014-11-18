4cc7d3cb8a37f9088ea893c2a3a19246616dd9f3
Bronson Oka <boka@slickage.com>
2014-11-17 15:54:16 -1000

    use local metadata scheme

--- test/test-treedb.js
+++ test/test-treedb.js
@@ -3,7 +3,7 @@ var async = require('async');
 var levelup = require('levelup');
 var treedb = require(path.join(__dirname, '..'));
 var db = new levelup(path.join('/', 'tmp', '.treedb'));
-var tree = treedb(db, {meta: require('epochmeta')});
+var tree = treedb(db, {meta: require(path.join(__dirname, 'meta'))});
 
 module.exports = tree;
 
