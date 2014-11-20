043c5e13eede678a759cc2310c2b85e4095a178a
Bronson Oka <boka@slickage.com>
2014-11-19 17:26:35 -1000

    update examples/query for new implementation

--- examples/query.js
+++ examples/query.js
@@ -2,7 +2,7 @@ var path = require('path');
 var db = require('levelup')(path.join('/', 'tmp', 'treedb'));
 var tree = require(path.join(__dirname, '..'))(db);
 var people = [];
-tree.nodes('person') // returns a readable stream
+tree.nodes({type: 'person'}) // returns a readable stream
 .on('data', function(person) {
   people.push(person);
 })
